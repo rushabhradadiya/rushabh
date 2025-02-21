286e97b800106a009ecab275ca9fc8e40a3f7ae7
rushabhradadiya <rushabhradadiya91@gmail.com>
2025-02-21 09:48:09 +0530

    Delete divisibleby10ornot.py

--- divisibleby10ornot.py
+++
@@ -1,8 +0,0 @@
-def check_divisible_by_10(num):
-    if num % 10 == 0:
-        return "Divisible by 10"
-    else:
-        return "Not divisible by 10"
-
-num = int(input("Enter a number: "))
-print(check_divisible_by_10(num))
